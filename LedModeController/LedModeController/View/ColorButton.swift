1aea62fbef22049b2e621909f3e5fc6bc40576c5
nafell <pixel68.43@gmail.com>
2025-03-25 00:12:09 +0900

    感情設定ボタンのViewを追加

---
+++ LedModeController/LedModeController/View/ColorButton.swift
@@ -0,0 +1,45 @@
+import Foundation
+import SwiftUI
+
+struct ColorButton: View {
+    var color: ColorModel
+    var action: (ColorModel) -> Void
+    var isSelected: Bool
+    
+    var body: some View {
+        Button(action: {
+            action(color)
+        }) {
+            Text(color.label)
+                .foregroundColor(
+                    isSelected ? Color.white : Color.black
+                )
+                .frame(width: 170, height: 80)
+                .font(
+                    .system(size: 24, weight: isSelected ? .black : .regular, design:.rounded)
+                )
+
+        }
+        .background(
+            isSelected ? Color(red: Double(color.red)/255, green: Double(color.green)/255, blue: Double(color.blue)/255) : Color.white
+        )
+        .overlay {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 4)
+                .fill(
+                    isSelected ? Color.white : Color(red: Double(color.red)/255, green: Double(color.green)/255, blue: Double(color.blue)/255)
+                )
+        }
+        .mask {
+            RoundedRectangle(cornerRadius: 20)
+        }
+        .padding([.leading, .trailing], 6)
+        .padding([.top, .bottom], 6)
+        .onHover(perform: { isHovered in
+            if (isHovered) {
+                action(color)
+            }
+        })
+            
+    }
+}
